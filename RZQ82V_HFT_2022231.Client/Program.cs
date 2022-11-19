b52c1ebd9f4b9248d854139851fcffdf49b4847d
pekarcsabi <pekarcsabi@gmail.com>
2022-11-19 14:48:10 +0100

    2019 VS test

--- RZQ82V_HFT_2022231.Client/Program.cs
+++ RZQ82V_HFT_2022231.Client/Program.cs
@@ -9,16 +9,7 @@ namespace RZQ82V_HFT_2022231.Client
     {
         static void Main(string[] args)
         {
-            FlyingDbContext ctx = new FlyingDbContext();
-            foreach (var item in ctx.Planes)
-            {
-                Console.WriteLine(item.Type);
-                foreach (var flight in item.Flights)
-                {
-                    Console.WriteLine("\t" + flight.FlightId + ": " + flight.Company.Name);
-                }
-            }
-            ;
+            //Test
         }
     }
 }
